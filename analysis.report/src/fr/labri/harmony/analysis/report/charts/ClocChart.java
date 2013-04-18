486cf558a6db6945ea35c93271a358af6441199e
Jean-Remy Falleri <jr.falleri@laposte.net>
2013-04-18 21:14:05 +0200

    update cloc chart.

--- analysis.report/src/fr/labri/harmony/analysis/report/charts/ClocChart.java
+++ analysis.report/src/fr/labri/harmony/analysis/report/charts/ClocChart.java
@@ -2,15 +2,15 @@ package fr.labri.harmony.analysis.report.charts;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.ShapeUtilities;
 
 import fr.labri.harmony.analysis.cloc.ClocEntries;
 import fr.labri.harmony.analysis.cloc.ClocEntry;
@@ -30,23 +30,23 @@ public class ClocChart extends ProduceChart {
 	@SuppressWarnings("deprecation")
 	@Override
 	public JFreeChart createChart(Source src) {
-		TimeSeriesCollection tset = new TimeSeriesCollection();
-		TimeSeries sevents = new TimeSeries("Java CLOC");
-		List<Event> events = new ArrayList<>(dao.getEvents(src));
+		XYSeriesCollection clocSeries = new XYSeriesCollection();
+		XYSeries java = new XYSeries("Java CLOC");
+		int nb = 0;
+		List<Event> events = new ArrayList<>(src.getEvents());
+		System.out.println(events.size());
 		Collections.sort(events, new EventComparator());
 		for(Event event: events) {
-			dao.refreshElement(event);
 			ClocEntries entries = dao.getData("cloc", ClocEntries.class, Data.EVENT, event.getId());
 			ClocEntry entry = entries.getEntry("Java");
-			Date devent = new Date(event.getTimestamp());
-			sevents.addOrUpdate(new Day(devent),entry.getCode());
+			java.add(nb, entry.getCode());
 		}
-		
-		tset.addSeries(sevents);
-		JFreeChart tchart = ChartFactory.createTimeSeriesChart("Number of items over time","Date","Items",tset,true,true,false);
-		XYLineAndShapeRenderer trend1 = (XYLineAndShapeRenderer) tchart.getXYPlot().getRenderer();
-		trend1.setShapesVisible(true);
-		return tchart;
+		clocSeries.addSeries(java);
+		JFreeChart chart = ChartFactory.createXYLineChart("Evolution of CLOC","Date","Items",clocSeries,PlotOrientation.HORIZONTAL,true,true,false);
+		XYSplineRenderer rend = new XYSplineRenderer();
+		rend.setShape(ShapeUtilities.createDiamond(1F));
+		chart.getXYPlot().setRenderer(rend);
+		return chart;
 	}
 
 	@Override
