3ba1dfad27b6db13671941d14c4b780d0cdac1f2
Marc Palyart <mpalyart@labri.fr>
2013-10-03 16:35:19 +0200

    Adding of comments on the workdir wizard

--- fr.labri.harmony.wizard.workdir/src/fr/labri/harmony/wizard/workdir/WorkingDirectoryWizardPage.java
+++ fr.labri.harmony.wizard.workdir/src/fr/labri/harmony/wizard/workdir/WorkingDirectoryWizardPage.java
@@ -4,8 +4,10 @@ package fr.labri.harmony.wizard.workdir;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 /**
  * Wizard Page to create a new Harmony analysis
@@ -18,7 +20,7 @@ public class WorkingDirectoryWizardPage extends WizardPage {
 	public WorkingDirectoryWizardPage() {
 	    super("New Harmony working directory");
 	    setTitle("New Harmony working directory");
-	    setDescription("This wizard creates a new Harmony working directory and provides and the necessary configuration files");
+	    setDescription("");
 	  }
 
 	@Override
@@ -26,6 +28,18 @@ public class WorkingDirectoryWizardPage extends WizardPage {
 		globalContainer = new Composite(parent, SWT.NULL);
 	    GridLayout layout = new GridLayout();
 	    globalContainer.setLayout(layout);
+	    
+	    Label pres = new Label(globalContainer, SWT.NULL);
+	    pres.setText("This wizard creates a new Harmony working directory and provides all the necessary configuration files");
+	    
+	    Label vSpace2 = new Label(globalContainer, SWT.NULL);
+	    vSpace2.setText("");
+	    GridData gridData = new GridData();
+		gridData.horizontalSpan = 3;
+		vSpace2.setLayoutData(gridData);
+	    
+	    Label instructions = new Label(globalContainer, SWT.NULL);
+	    instructions.setText("Click on Finish. Then type harmony in the OSGi console");    
 	    
 	    setControl(globalContainer);
 	    setPageComplete(true);
