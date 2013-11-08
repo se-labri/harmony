d099e6de9f342783c2063ca8777c5622b22d35d0
Matthieu Foucault <matthieu.foucault@labri.fr>
2013-11-08 11:40:03 +0100

    Cleaning and adding methods in Dao

--- fr.labri.harmony.core/src/fr/labri/harmony/core/dao/AbstractDao.java
+++ fr.labri.harmony.core/src/fr/labri/harmony/core/dao/AbstractDao.java
@@ -1,8 +1,17 @@
 package fr.labri.harmony.core.dao;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import fr.labri.harmony.core.AbstractHarmonyService;
+import fr.labri.harmony.core.model.Source;
+import fr.labri.harmony.core.model.SourceElement;
 
 public abstract class AbstractDao {
 	
@@ -29,6 +38,73 @@ public abstract class AbstractDao {
 
 	public EntityManager getEntityManager() {
 		return getEntityManager(HARMONY_PERSISTENCE_UNIT);
+	}
+	
+	public synchronized HarmonyEntityManagerFactory getEntityManagerFactory(AbstractHarmonyService harmonyService) {
+		String puName;
+		if (harmonyService == null) puName = HARMONY_PERSISTENCE_UNIT;
+		else puName = harmonyService.getPersitenceUnitName();
+		return entityManagerFactories.get(puName);
+	}
+	
+	/*******************
+	 * Generic Methods *
+	 *******************/
+	
+	protected <E> void save(Collection<E> elements) {
+		EntityManager m = getEntityManager();
+		if (!m.getTransaction().isActive()) m.getTransaction().begin();
+		for (E e : elements) {
+			m.persist(e);
+		}
+		m.getTransaction().commit();
+		m.close();
+	}
+	
+	/**
+	 * 
+	 * @param clazz
+	 * @param s
+	 * @param nativeId
+	 * @return
+	 */
+	protected <E extends SourceElement> E get(Class<E> clazz, Source s, String nativeId) {
+		EntityManager m = getEntityManager();
+		m.getTransaction().begin();
+		String sQuery = "SELECT e FROM " + clazz.getSimpleName() + " e WHERE e.source.id = :sourceId AND e.nativeId = :nativeId";
+		TypedQuery<E> query = m.createQuery(sQuery, clazz);
+		query.setParameter("sourceId", s.getId());
+		query.setParameter("nativeId", nativeId).setMaxResults(1);
+		E result = null;
+		try {
+			result = query.getSingleResult();
+		} catch (NoResultException ex) {
+
+		}
+		m.getTransaction().commit();
+		return result;
+	}
+
+	protected <E extends SourceElement> List<E> getList(Class<E> clazz, Source s) {
+		EntityManager m = getEntityManager();
+		String sQuery = "SELECT e FROM " + clazz.getSimpleName() + " e WHERE e.source.id = :sourceId AND e.nativeId = :nativeId";
+		TypedQuery<E> query = m.createQuery(sQuery, clazz);
+		query.setParameter("sourceId", s.getId());
+		try {
+			return query.getResultList();
+		} catch (NoResultException ex) {
+			return new ArrayList<>();
+		}
+	}
+
+	protected <T> T refreshElement(T element) {
+		EntityManager m = getEntityManager();
+		m.getTransaction().begin();
+		element = m.merge(element);
+		m.refresh(element);
+		m.getTransaction().commit();
+
+		return element;
 	}
 	
 	/**
