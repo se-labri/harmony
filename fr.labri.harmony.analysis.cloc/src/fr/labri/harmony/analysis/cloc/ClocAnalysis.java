24f0a498b8f7bc0fa25a45a5c16aa8fe2e63aa15
Matthieu Foucault <matthieu.foucault@labri.fr>
2013-11-21 10:34:35 +0100

    Minor changes in cloc and ownership

--- fr.labri.harmony.analysis.cloc/src/fr/labri/harmony/analysis/cloc/ClocAnalysis.java
+++ fr.labri.harmony.analysis.cloc/src/fr/labri/harmony/analysis/cloc/ClocAnalysis.java
@@ -39,43 +39,49 @@ public class ClocAnalysis extends AbstractAnalysis {
 		for (Event ev : src.getEvents()) {
 			src.getWorkspace().update(ev);
 			String workspacePath = src.getWorkspace().getPath();
-			try {
-				Process p = Runtime.getRuntime().exec(new String[] { "cloc", new File(workspacePath).getAbsolutePath(), "--xml", "--quiet" });
-				BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				StringBuffer b = new StringBuffer();
-				String line;
-				while ((line = r.readLine()) != null) {
-					if (line.trim().startsWith("<")) b.append(line + "\n");
-				}
-				p.waitFor();
-				r.close();
+			ClocEntries entries = runCloc(workspacePath);
+			if (entries != null) dao.saveData(this.getPersitenceUnitName(), entries, ev);
 
-				DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
-				DocumentBuilder db = df.newDocumentBuilder();
-
-				Document doc = db.parse(new InputSource(new StringReader(b.toString().trim())));
-				NodeList l = doc.getElementsByTagName("language");
-
-				ClocEntries entries = new ClocEntries();
-				for (int i = 0; i < l.getLength(); i++) {
-					Node el = l.item(i);
-					final NamedNodeMap attributes = el.getAttributes();
-					String lang = attributes.getNamedItem("name").getTextContent();
-					ClocEntry e = new ClocEntry();
-					e.setLanguage(lang);
-					e.setCode(Integer.parseInt(attributes.getNamedItem("code").getTextContent()));
-					e.setFile(Integer.parseInt(attributes.getNamedItem("files_count").getTextContent()));
-					e.setBlank(Integer.parseInt(attributes.getNamedItem("blank").getTextContent()));
-					e.setComment(Integer.parseInt(attributes.getNamedItem("comment").getTextContent()));
-					entries.getEntries().add(e);
-				}
-
-				dao.saveData(this.getPersitenceUnitName(), entries, ev);
-				p.waitFor();
-			} catch (Exception ex) {
-				throw new WorkspaceException(ex);
-			}
 		}
+	}
+
+	private ClocEntries runCloc(String workspacePath) {
+		ClocEntries entries = null;
+		try {
+			Process p = Runtime.getRuntime().exec(new String[] { "cloc", new File(workspacePath).getAbsolutePath(), "--xml", "--quiet" });
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuffer b = new StringBuffer();
+			String line;
+			while ((line = r.readLine()) != null) {
+				if (line.trim().startsWith("<")) b.append(line + "\n");
+			}
+			p.waitFor();
+			r.close();
+
+			DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = df.newDocumentBuilder();
+
+			Document doc = db.parse(new InputSource(new StringReader(b.toString().trim())));
+			NodeList l = doc.getElementsByTagName("language");
+
+			entries = new ClocEntries();
+			for (int i = 0; i < l.getLength(); i++) {
+				Node el = l.item(i);
+				final NamedNodeMap attributes = el.getAttributes();
+				String lang = attributes.getNamedItem("name").getTextContent();
+				ClocEntry e = new ClocEntry();
+				e.setLanguage(lang);
+				e.setCode(Integer.parseInt(attributes.getNamedItem("code").getTextContent()));
+				e.setFile(Integer.parseInt(attributes.getNamedItem("files_count").getTextContent()));
+				e.setBlank(Integer.parseInt(attributes.getNamedItem("blank").getTextContent()));
+				e.setComment(Integer.parseInt(attributes.getNamedItem("comment").getTextContent()));
+				entries.getEntries().add(e);
+			}
+
+		} catch (Exception ex) {
+			throw new WorkspaceException(ex);
+		}
+		return entries;
 	}
 
 }
